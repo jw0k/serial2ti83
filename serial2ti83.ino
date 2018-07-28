a4af72250ab8decab83a6db60f49f43b2cc26b16
Matthew Trescott <matthewtrescott@gmail.com>
2018-07-28 16:42:45 -0400

    Use direct port access rather than Arduino's functions. This vastly improves TI-86 compatibility

--- serial2ti83.ino
+++ serial2ti83.ino
@@ -1,7 +1,58 @@
-const int TIP = 2; //arduino port where the tip of the stereo jack plug is connected
-const int RING = 3; //arduino port where the middle part of the stereo jack plug is connected
-const unsigned long TXTIMEOUT = 50; //ms //change to 80 for ti83 / 82 stats
-const unsigned long RXTIMEOUT = 5; //ms //change to 10 ...
+/* ArduGrayLink, a modern yet simple TI Graph-Link cable
+ *  https://github.com/MTres19/serial2ti83
+ *  https://github.com/DSchndr/serial2ti83
+ *  https://github.com/jw0k/serial2ti83
+ * 
+ * Copyright (C) 2017 jw0k
+ * Copyright (C) 2018 DSchndr
+ * Copyright (C) 2018 Matthew Trescott
+ * 
+ * MIT-licensed.
+ */
+
+const int TIP = A0; //arduino port where the tip of the stereo jack plug is connected
+const int RING = A1; //arduino port where the middle part of the stereo jack plug is connected
+const unsigned long TXTIMEOUT = 50; //ms
+const unsigned long RXTIMEOUT = 20; //ms
+
+#define TI_TIP_IS_HIGH   (PINC & (uint8_t)0b00000001) // No need for bitshift because it's the first pin.
+#define TI_RING_IS_HIGH ((PINC & (uint8_t)0b00000010) >> 1)
+
+#define TI_TIP_IS_LOW   ((~PINC) & (uint8_t)0b00000001) // No need for bitshift because it's the first pin.
+#define TI_RING_IS_LOW (((~PINC) & (uint8_t)0b00000010) >> 1)
+
+inline void TiTipLow()
+{
+    PORTC &= (uint8_t)0b11111110; // Save an instruction https://www.nongnu.org/avr-libc/user-manual/FAQ.html#faq_intpromote
+}
+inline void TiRingLow()
+{
+    PORTC &= (uint8_t)0b11111101;
+}
+inline void TiTipHigh()
+{
+    PORTC |= (uint8_t)0b00000001;
+}
+inline void TiRingHigh()
+{
+    PORTC |= (uint8_t)0b00000010;
+}
+inline void TiTipOutput()
+{
+    DDRC |= (uint8_t)0b00000001;
+}
+inline void TiRingOutput()
+{
+    DDRC |= (uint8_t)0b00000010;
+}
+inline void TiTipInput()
+{
+    DDRC &= (uint8_t)0b11111110;
+}
+inline void TiRingInput()
+{
+    DDRC &= (uint8_t)0b11111101;
+}
 
 void sendByte(uint8_t byte)
 {
@@ -14,7 +65,7 @@ void sendByte(uint8_t byte)
 
         //poll both lines until they are both high, which means we're ready to send a bit
         currentTime = millis();
-        while (digitalRead(TIP) == LOW || digitalRead(RING) == LOW)
+        while (TI_TIP_IS_LOW || TI_RING_IS_LOW)
         {
             if (millis() - currentTime > TXTIMEOUT)
             {
@@ -22,44 +73,76 @@ void sendByte(uint8_t byte)
             }
         }
 
-        int ourLine;
-        int oppositeLine;
+        //int ourLine;
+        //int oppositeLine;
         if (bit)
         {
-            ourLine = RING;
-            oppositeLine = TIP;
+            //ourLine = RING;
+            //oppositeLine = TIP;
+
+            // send a bit by pulling appropriate line low
+            TiRingOutput();
+            TiRingLow();
+
+            // wait for opposite line to become low
+            currentTime = millis();
+            while (TI_TIP_IS_HIGH)
+            {
+                if (millis() - currentTime > TXTIMEOUT)
+                {
+                    TiRingInput();
+                    TiRingHigh();
+                    return;
+                }
+            }
+
+            // release our line
+            TiRingInput();
+            TiRingHigh();
+
+            // wait for opposite line to become high
+            currentTime = millis();
+            while (TI_TIP_IS_LOW)
+            {
+                if (millis() - currentTime > TXTIMEOUT)
+                {
+                    return;
+                }
+            }
         }
         else
         {
-            ourLine = TIP;
-            oppositeLine = RING;
-        }
+            //ourLine = TIP;
+            //oppositeLine = RING;
 
-        //send a bit by pulling appropriate line low
-        pinMode(ourLine, OUTPUT);
-        digitalWrite(ourLine, LOW);
+            // send a bit by pulling appropriate line low
+            TiTipOutput();
+            TiTipLow();
 
-        //wait for opposite line to become low
-        currentTime = millis();
-        while (digitalRead(oppositeLine) == HIGH)
-        {
-            if (millis() - currentTime > TXTIMEOUT)
+            // wait for opposite line to become low
+            currentTime = millis();
+            while (TI_RING_IS_HIGH)
             {
-                pinMode(ourLine, INPUT_PULLUP);
-                return;
+                if (millis() - currentTime > TXTIMEOUT)
+                {
+                    TiTipInput();
+                    TiTipHigh();
+                    return;
+                }
             }
-        }
 
-        //release our line
-        pinMode(ourLine, INPUT_PULLUP);
+            // release our line
+            TiTipInput();
+            TiTipHigh();
 
-        //wait for opposite line to become high
-        currentTime = millis();
-        while (digitalRead(oppositeLine) == LOW)
-        {
-            if (millis() - currentTime > TXTIMEOUT)
+            // wait for opposite line to become high
+            currentTime = millis();
+            while (TI_RING_IS_LOW)
             {
-                return;
+                if (millis() - currentTime > TXTIMEOUT)
+                {
+                    return;
+                }
             }
         }
     }
@@ -69,52 +152,71 @@ bool getByte(uint8_t& byte)
 {
     unsigned long currentTime;
     uint8_t result = 0;
-
     for (int i = 0; i < 8; ++i)
     {
         //poll both lines until one of them becomes low
+        delayMicroseconds(6); // Why is this needed after I replaced digitalRead() in the while loop below?
         currentTime = millis();
-        while (digitalRead(TIP) == HIGH && digitalRead(RING) == HIGH)
+        while (TI_RING_IS_HIGH && TI_TIP_IS_HIGH)
         {
             if (millis() - currentTime > RXTIMEOUT)
             {
                 return false;
             }
         }
-
-        int ourLine;
-        int oppositeLine;
-        bool bit = (digitalRead(RING) == LOW);
+        
+        bool bit = TI_RING_IS_LOW;
         result >>= 1;
+        
         if (bit)
         {
-            ourLine = TIP;
-            oppositeLine = RING;
+            //ourLine = TIP;
+            //oppositeLine = RING;
             result |= 0x80; //bits are always transmitted LSb first (least significant bit)
+            
+            // acknowledge a bit by pulling appropriate line low
+            TiTipOutput();
+            TiTipLow();
+            
+            //wait for opposite line to become high
+            currentTime = millis();
+            while (TI_RING_IS_LOW)
+            {
+                if (millis() - currentTime > RXTIMEOUT)
+                {
+                    TiTipInput();
+                    TiTipHigh();
+                    return false;
+                }
+            }
+
+            // release our line
+            TiTipInput();
+            TiTipHigh();
         }
         else
         {
-            ourLine = RING;
-            oppositeLine = TIP;
-        }
-
-        //acknowledge a bit by pulling appropriate line low
-        pinMode(ourLine, OUTPUT);
-        digitalWrite(ourLine, LOW);
-
-        //wait for opposite line to become high
-        currentTime = millis();
-        while (digitalRead(oppositeLine) == LOW)
-        {
-            if (millis() - currentTime > RXTIMEOUT)
+            //ourLine = RING;
+            //oppositeLine = TIP;
+            
+            // acknowledge a bit by pulling appropriate line low
+            TiRingOutput();
+            TiRingLow();
+            
+            //wait for opposite line to become high
+            while (TI_TIP_IS_LOW)
             {
-                pinMode(ourLine, INPUT_PULLUP);
-                return false;
+                if (millis() - currentTime > RXTIMEOUT)
+                {
+                    TiRingInput();
+                    TiRingHigh();
+                }
             }
-        }
 
-        //release our line
-        pinMode(ourLine, INPUT_PULLUP);
+            // release our line
+            TiRingInput();
+            TiRingHigh();
+        }
     }
 
     byte = result;
@@ -128,11 +230,14 @@ void setup()
     digitalWrite(LED_BUILTIN, LOW);
 
     Serial.begin(9600);
-    Serial.setTimeout(10); // set serialtimeout lower for arduino w/o serial converter.
 
     //configure both lines to be in a high-impedance state and enable pull-up resistors
-    pinMode(TIP, INPUT_PULLUP);
-    pinMode(RING, INPUT_PULLUP);
+    //pinMode(TIP, INPUT_PULLUP);
+    //pinMode(RING, INPUT_PULLUP);
+    TiTipInput();
+    TiTipHigh();
+    TiRingInput();
+    TiRingHigh();
 }
 
 void loop()
@@ -140,6 +245,9 @@ void loop()
     //forward incoming data from PC to the calculator
     while (Serial.available() > 0)
     {
+        if (Serial.available() > 32) {
+          digitalWrite(LED_BUILTIN, HIGH);
+        }
         sendByte(Serial.read());
     }
 
